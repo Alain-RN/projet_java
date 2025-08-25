7835a6aae06001b583d5a6b268cc3922f4faaac6
Noelisoa Alain <alainrakotoarivony926@gmail.com>
2025-08-25 15:12:00 +0300

    form

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -7,26 +7,4 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
 
-    @FXML
-    private TextField usernameField;
-
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private Label statusLabel;
-
-    @FXML
-    private void login() {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            statusLabel.setText("Veuillez remplir tous les champs !");
-        } else if (username.equals("admin") && password.equals("1234")) {
-            statusLabel.setText("Connexion réussie !");
-        } else {
-            statusLabel.setText("Nom d'utilisateur ou mot de passe incorrect !");
-        }
-    }
 }
